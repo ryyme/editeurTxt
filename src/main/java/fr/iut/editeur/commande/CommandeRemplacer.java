5dfe230b5a99d71f7cf9c907a47c67ff27d583aa
lorenzinir <remy.lorenzini@etu.umontpellier.fr>
2023-11-07 13:34:11 +0100

    ajout constructeur et executer

--- src/main/java/fr/iut/editeur/commande/CommandeRemplacer.java
+++ src/main/java/fr/iut/editeur/commande/CommandeRemplacer.java
@@ -1,4 +1,24 @@
 package fr.iut.editeur.commande;
 
-public class CommandeRemplacer {
+import fr.iut.editeur.document.Document;
+
+public class CommandeRemplacer extends CommandeDocument{
+    public CommandeRemplacer(Document document, String[] parameters) {
+        super(document, parameters);
+    }
+
+    @Override
+    public void executer(){
+        if(parameters.length < 4) {
+            System.err.println("Format remplacer;depart;fin;chaine");
+            return;
+        }
+        int debut = Integer.parseInt(parameters[1]);
+        int fin = Integer.parseInt(parameters[2]);
+        String texte = parameters[3];
+
+        this.document.remplacer(debut,fin,texte);
+        super.executer();
+    }
+
 }
